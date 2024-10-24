c50fcb6c7c5ed0e8e7780f08da4e35eac7c00c5f
KangMinGyo <kang15567@kakao.com>
2024-10-24 21:56:54 +0900

    design: Add UI Design

--- TurnipForecast/Controllers/ViewController.swift
+++ TurnipForecast/Controllers/ViewController.swift
@@ -9,9 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet private weak var purchasePriceLabel: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
 
