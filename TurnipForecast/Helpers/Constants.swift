6dd0aff4ca52a5896b57f7f5bdea938fd4e6004b
KangMinGyo <kang15567@kakao.com>
2024-10-29 14:35:12 +0900

    feat: add Constants

---
+++ TurnipForecast/Helpers/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  TurnipForecast
+//
+//  Created by KangMingyo on 10/29/24.
+//
+
+import Foundation
--- TurnipForecast/Networking/NetworkManager.swift
+++ TurnipForecast/Networking/NetworkManager.swift
@@ -7,6 +7,18 @@
 
 import Foundation
 
+//MARK: - 네트워크에서 발생할 수 있는 에러 정의
+enum NetworkError: Error {
+    case networkingError
+    case dataError
+    case parseError
+}
+
 final class NetworkManager {
     
+    typealias NetworkCompletion = (Result<[TurnipPriceData], NetworkError>) -> Void
+
+    func fetchTurnipPriceData(prices: [Int], completion: @escaping NetworkCompletion) {
+        
+    }
 }
