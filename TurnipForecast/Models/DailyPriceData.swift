e9820df35a52e10fee34f5b52232283730d0ec96
KangMinGyo <kang15567@kakao.com>
2024-10-31 17:15:13 +0900

    feat: add convertFromTurnipPriceData to DailyPriceData

--- TurnipForecast/Models/DailyPriceData.swift
+++ TurnipForecast/Models/DailyPriceData.swift
@@ -7,6 +7,34 @@
 
 import Foundation
 
-struct DailyPriceData {
-    
+struct DailyPriceData: Identifiable {
+    let id = UUID() // 고유 식별자 추가
+    let day: String
+    let minDay: Double
+    let maxDay: Double
+    let minNight: Double
+    let maxNight: Double
+}
+
+extension DailyPriceData {
+    static func convertFromTurnipPriceData(turnipPriceData: TurnipPriceData) -> [DailyPriceData] {
+        let weekDays = ["월요일", "화요일", "수요일", "목요일", "금요일", "토요일"]
+        
+        var dailyPriceDataArray: [DailyPriceData] = []
+        
+        for (index, pattern) in turnipPriceData.minMaxPattern.enumerated() {
+            if index < weekDays.count {
+                let dailyPriceData = DailyPriceData(
+                    day: weekDays[index],
+                    minDay: Double(pattern[0]),
+                    maxDay: Double(pattern[1]),
+                    minNight: Double(pattern[0]),
+                    maxNight: Double(pattern[1])
+                )
+                dailyPriceDataArray.append(dailyPriceData)
+            }
+        }
+        
+        return dailyPriceDataArray
+    }
 }
--- TurnipForecast/Views/ChartView.swift
+++ TurnipForecast/Views/ChartView.swift
@@ -8,23 +8,14 @@
 import SwiftUI
 import Charts
 
-struct DayData: Identifiable {
-    let id = UUID() // 고유 식별자 추가
-    let day: String
-    let minMorning: Double
-    let maxMorning: Double
-    let minAfternoon: Double
-    let maxAfternoon: Double
-}
-
 struct ChartView: View {
-    let data: [DayData] = [
-        DayData(day: "월요일", minMorning: 10, maxMorning: 20, minAfternoon: 15, maxAfternoon: 25),
-        DayData(day: "화요일", minMorning: 12, maxMorning: 22, minAfternoon: 17, maxAfternoon: 27),
-        DayData(day: "수요일", minMorning: 8, maxMorning: 18, minAfternoon: 14, maxAfternoon: 24),
-        DayData(day: "목요일", minMorning: 11, maxMorning: 21, minAfternoon: 13, maxAfternoon: 23),
-        DayData(day: "금요일", minMorning: 14, maxMorning: 19, minAfternoon: 16, maxAfternoon: 26),
-        DayData(day: "토요일", minMorning: 9, maxMorning: 15, minAfternoon: 12, maxAfternoon: 22)
+    let data: [DailyPriceData] = [
+        DailyPriceData(day: "월요일", minMorning: 10, maxMorning: 20, minAfternoon: 15, maxAfternoon: 25),
+        DailyPriceData(day: "화요일", minMorning: 12, maxMorning: 22, minAfternoon: 17, maxAfternoon: 27),
+        DailyPriceData(day: "수요일", minMorning: 8, maxMorning: 18, minAfternoon: 14, maxAfternoon: 24),
+        DailyPriceData(day: "목요일", minMorning: 11, maxMorning: 21, minAfternoon: 13, maxAfternoon: 23),
+        DailyPriceData(day: "금요일", minMorning: 14, maxMorning: 19, minAfternoon: 16, maxAfternoon: 26),
+        DailyPriceData(day: "토요일", minMorning: 9, maxMorning: 15, minAfternoon: 12, maxAfternoon: 22)
     ]
 
     var body: some View {
